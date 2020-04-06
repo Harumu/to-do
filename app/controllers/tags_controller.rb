71c62b8387e00fbbc75978ac1c8df9d70f9993aa
Harumu <kokusan.komugiko120g@gmail.com>
2020-04-06 06:46:20 +0000

    10

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,65 +1,59 @@
 class TagsController < ApplicationController
-  before_action :authenticate_user!
+ before_action :authenticate_user!
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
-  # GET /tags
-  # GET /tags.json
+   # GET /tags
   def index
-    @tags = current_user.tag.all
+   @tags = current_user.tags.all
   end
 
-  # GET /tags/1
-  # GET /tags/1.json
-  def show
-  end
+   # # GET /tags/1
+  # def show
+  # end
 
-  # GET /tags/new
+   # GET /tags/new
   def new
-    @tag = current_user.tags.new
+   @tag = current_user.tags.new
   end
 
-  # GET /tags/1/edit
+   # GET /tags/1/edit
   def edit
   end
 
-  # POST /tags
-  # POST /tags.json
+   # POST /tags
   def create
-    @tag = current_user.tags.new(tag_params)
+   @tag = current_user.tags.new(tag_params)
 
-    respond_to do |format|
-      if @tag.save
-        @status = true
-      else
-        @status = false
-      end
+     if @tag.save
+     @status = true
+    else
+     @status = false
+    end
   end
 
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
+   # PATCH/PUT /tags/1
   def update
-      if @tag.update(tag_params)
-        @status = true
-      else
-        @status = false
-      end
+    if @tag.update(tag_params)
+     @status = true
+    else
+     @status = false
+    end
   end
 
-  # DELETE /tags/1
-  # DELETE /tags/1.json
+   # DELETE /tags/1
   def destroy
     @tag.destroy
-    
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tag
-      @tag = current_user.tags.find(params[:id])
-    end
+   private
 
-    # Only allow a list of trusted parameters through.
-    def tag_params
-      params.require(:tag).permit(:name, :user_id)
-    end
+   # Use callbacks to share common setup or constraints between actions.
+  def set_tag
+   @tag = current_user.tags.find(params[:id])
+  end
+
+   # Only allow a trusted parameter "white list" through.
+  def tag_params
+    params.require(:tag).permit(:name, :user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  def available_tags
+  self.tags.select(&:persisted?)
+end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
